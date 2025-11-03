326eac5e4a9ca96781f464d270d2cec73a75ba77
Alex <pallarete@gmail.com>
2025-11-03 19:51:43 +0100

    datatypes

---
+++ 01_basic/02_types.py
@@ -0,0 +1,34 @@
+# 01 - types()
+# Python tiene varios tipos de datos
+# int, float, complex, str, bool, Nonetype,
+# list, tuple, range, set 
+import os
+os.system('cls')
+print('int:')
+# print(type(18))
+# print(type(0))
+# print(type(-5))
+# print(type(4564564564564564564654))
+print('float: ')
+# print(type(3.14))
+# print(type(1.0))
+# print(type(1e3))
+
+print('complex: ')
+# print(type(1 + 2j))
+
+print('str:')
+# print(type('Hola'))
+# print(type(''))
+# print(type('123'))
+# print(type('''
+# Multilinea
+# '''))
+
+print('bool:')
+# print(type(True))
+# print(type(False))
+# print(type(1<2))
+
+print('NoneType:')
+print(type(None))
