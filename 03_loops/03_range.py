d9b633b8ed46739e9769bc40640448ea5d8ab7f9
Alex <pallarete@gmail.com>
2025-11-13 11:50:50 +0100

    exercises

--- 03_loops/03_range.py
+++ 03_loops/03_range.py
@@ -39,25 +39,43 @@ for _ in range(5):
 
 # Ejercicio 1: Imprimir números del 1 al 10
 # Imprime los números del 1 al 10 (inclusive) usando un bucle for y range().
-print("\nEjercicio 1:")
+print("\nEjercicio 1: Imprimir números del 1 al 10 ")
+for numero in range(1, 11):
+  print(numero)
+
 
 # Ejercicio 2: Imprimir números impares del 1 al 20
 # Imprime todos los números impares entre 1 y 20 (inclusive) usando un bucle for y range().
-print("\nEjercicio 2:")
+print("\nEjercicio 2: Imprimir números impares del 1 al 20 ")
+for numero in range(1,21):
+  if numero %2 != 0:
+    print(numero)
 
 # Ejercicio 3: Imprimir múltiplos de 5
 # Imprime los múltiplos de 5 desde 5 hasta 50 (inclusive) usando un bucle for y range().
-print("\nEjercicio 3:")
+print("\nEjercicio 3: Imprimir múltiplos de 5")
+for numero in range(5,51):
+  if numero % 5 ==0:
+    print(numero)
 
 # Ejercicio 4: Imprimir números en orden inverso
 # Imprime los números del 10 al 1 (inclusive) en orden inverso usando un bucle for y range().
-print("\nEjercicio 4:")
+print("\nEjercicio 4: Imprimir números en orden inverso ")
+for numero in range(10, 0, -1):
+  print(numero)
 
 # Ejercicio 5: Suma de números en un rango
 # Calcula la suma de los números del 1 al 100 (inclusive) usando un bucle for y range().
-print("\nEjercicio 5:")
+print("\nEjercicio 5: Suma de números en un rango ")
+suma =0
+for numero in range(1, 101):
+  suma+= numero
+print(suma)
 
 # Ejercicio 6: Tabla de multiplicar
 # Pide al usuario que introduzca un número.
 # Imprime la tabla de multiplicar de ese número (del 1 al 10) usando un bucle for y range().
-print("\nEjercicio 6:")
+print("\nEjercicio 6: Tabla de multiplicar ")
+multiplicador = int(input('Introduce un numero: '))
+for numero in range(1,11):
+  print(f'{multiplicador} * {numero} = { multiplicador * numero}')
